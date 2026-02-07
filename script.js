51b51d8fc736d6a0fa543c49e5b0f72d98e0f0ba
reenadanikabendukuri <reenadanikabendukuri@gmail.com>
2026-02-07 21:36:38 +0000

    day 1

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+console.log(4+5);
+console.log('4'+5);
+console.log(5*-3);
+console.log(5*'-3');
+
+let isRaining = true;
+let isCold = true;
+if (isRaining && isCold)
+{
+    console.log("Wear a jacket and take an umbrella!");
+}
+
+let time= 7;
+if (time>6 && time<12)
+{
+    console.log("Good morning!");
+}
